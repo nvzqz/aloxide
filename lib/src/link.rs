7f5b71b214903e7a5f2a5f2be1347e4b060cc7db
Nikolai Vazquez <nikvzqz@gmail.com>
2019-05-12 09:19:02 +0200

    Make linking less error prone

--- lib/src/link.rs
+++ lib/src/link.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use crate::{Ruby, RubyExecError};
 use RubyLinkError::*;
 
@@ -13,69 +14,80 @@ fn link_framework(lib: &str) {
     println!("cargo:rustc-link-lib=framework={}", lib);
 }
 
+// e.g. "-llibruby"
+fn lib_name(lib_flag: &str) -> &str {
+    &lib_flag[2..]
+}
+
+// e.g. "user32.lib"
+fn lib_name_msvc(lib_flag: &str) -> &str {
+    &lib_flag[..(lib_flag.len() - 4)]
+}
+
 pub(crate) fn link(ruby: &Ruby, static_lib: bool) -> Result<(), RubyLinkError> {
     println!("cargo:rustc-link-search=native={}", ruby.lib_path.display());
+
+    let target = ruby.get_config("target")?;
+    let target_msvc = target.contains("msvc") || target.contains("mswin");
+    let lib_name = if target_msvc { lib_name_msvc } else { lib_name };
 
     let key = if static_lib {
         "LIBRUBYARG_STATIC"
     } else {
         "LIBRUBYARG_SHARED"
     };
-
-    let libs = ruby.aux_libs(static_lib)?;
-    let libs = libs.as_str();
     let args = ruby.get_config(key)?;
 
     if args.trim().is_empty() {
         return Err(RubyLinkError::MissingLibs { static_lib });
     }
 
-    let link_lib = if static_lib { link_static } else { link_dynamic };
+    let so_libs = ruby.so_libs()?;
+    let aux_libs = ruby.aux_libs(static_lib)?;
 
-    let ruby_lib = ruby.lib_name(static_lib)?;
-    link_lib(&ruby_lib);
-
-    let link_lib = move |lib| {
-        if lib != ruby_lib && !libs.contains(lib) {
-            link_lib(lib);
-        }
+    // TODO: `MAINLIBS` can be `nil` on Windows, and so `aux_libs()` should make
+    // use of `Option<String>` instead
+    let aux_libs = if aux_libs != "nil" {
+        aux_libs.as_str()
+    } else {
+        ""
     };
 
-    let target = ruby.get_config("target")?;
+    let mut dy_libs = HashSet::new();
+    dy_libs.extend(aux_libs.split_ascii_whitespace().map(lib_name));
+    dy_libs.extend(so_libs.split_ascii_whitespace().map(lib_name));
 
-    // Compile for MSVC toolchain based on a `rustc` target or Ruby target
-    if target.contains("msvc") || target.contains("mswin") {
-        fn link_libs<'a>(libs: &'a str, link: impl Fn(&'a str) -> ()) {
-            for lib in libs.split_ascii_whitespace() {
-                if lib.ends_with(".lib") {
-                    let name_len = lib.len() - 4;
-                    link(&lib[..name_len]);
-                } else {
-                    panic!("Unknown arg {:?} in {:?}", lib, libs);
-                }
-            }
-        }
-        link_libs(&libs, link_dynamic);
-        link_libs(&args, link_lib);
+    let ruby_lib = ruby.lib_name(static_lib)?;
+    if static_lib {
+        link_static(&ruby_lib);
+    } else {
+        link_dynamic(&ruby_lib);
+    }
+
+    let seen_lib = |lib: &str| {
+        lib == ruby_lib || dy_libs.contains(lib)
+    };
+
+    for lib in &dy_libs {
+        link_dynamic(lib);
+    }
+
+    // TODO: Figure out whether `args` should be evaluated for MSVC
+    if target_msvc {
         return Ok(());
     }
 
-    // Ruby's dependencies should all be linked dynamically
-    for lib in libs.split_ascii_whitespace() {
-        link_dynamic(&lib[2..]);
-    }
+    // Need to call `next()` in "-framework" case
+    let mut args_iter = args.split_ascii_whitespace();
 
-    let mut iter = args.split_ascii_whitespace();
-
-    // Need to call `iter.next()` in `-framework` case
-    while let Some(arg) = iter.next() {
+    while let Some(arg) = args_iter.next() {
         if arg.len() < 2 {
             panic!("Unknown arg {:?} in {:?}", arg, args);
         }
         let (opt, val) = arg.split_at(2);
         match opt {
-            "-l" => if !libs.contains(opt) {
-                panic!("Found unexpected lib {:?} in {:?}", val, args);
+            "-l" => if !seen_lib(val) {
+                link_dynamic(val);
             },
             "-L" => {
                 println!("cargo:rustc-link-search=native={}", val);
@@ -87,7 +99,7 @@ pub(crate) fn link(ruby: &Ruby, static_lib: bool) -> Result<(), RubyLinkError> {
                 continue;
             },
             _ => if arg == "-framework" {
-                let framework = match iter.next() {
+                let framework = match args_iter.next() {
                     Some(arg) => arg,
                     None => return Err(MissingFramework(args)),
                 };
